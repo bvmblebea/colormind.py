037eb7fc4a0d76ef3a7315081f1fd2ea272b9d75
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-11 14:57:56 +0600

    Create colormind.py

---
+++ src/colormind.py
@@ -0,0 +1,36 @@
+import requests
+
+
+class ColorMind:
+	def __init__(self):
+		self.api = "http://colormind.io"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36"
+		}
+	
+	def get_random_color_palette(self, model: str = "default"):
+		data = {
+			"model": model
+		}
+		return requests.post(
+			f"{self.api}/api/",
+			data=data,
+			headers=self.headers).json()
+	
+	def get_color_suggestions(
+			self, 
+			input: list,
+			model: str = "default"):
+		data = {
+			"input": input,
+			"model": model
+		}
+		return requests.post(
+			f"{self.api}/api/",
+			data=data,
+			headers=self.headers).json()
+	
+	def get_models_list(self):
+		return requests.get(
+			f"{self.api}/list/",
+			headers=self.headers).json()
